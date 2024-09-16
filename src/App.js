45499f5770f99b889e6dae7ff7eb0f119a0d9c4b
GiovaniAraujo97 <giovanijesus1997@gmail.com>
2024-09-16 16:12:55 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './components/home/home';
+import Sobre from './components/sobre/sobre'
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Sobre />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home/home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../navbar/navbar";
+import Container from "../container/container";
+import "./home.css"
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Container />
+        </div>
+    )
+}
+
+export default Home;
